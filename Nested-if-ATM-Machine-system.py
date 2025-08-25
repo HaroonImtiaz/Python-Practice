7b079a53ced09ae84c0063e0b3dd1230f58ef46a
Haroon Imtiaz <haroonimtiaz832@gmail.com>
2025-08-25 10:01:34 -0700

    Create Nested-if-ATM-Machine-system.py

---
+++ Nested-if-ATM-Machine-system.py
@@ -0,0 +1,9 @@
+card_valid = input("please enter your valid card no yes/no:")
+if card_valid == "yes":
+    pin = int(input("Please enter your four digit pin:"))
+    if pin == 4884:
+        print("Transaction allowed")
+    else:
+        print("wrong pin")
+else:
+    print("invalid card")
