8001da9d7961cb9077fa541e6400042acb18c6cd
Haroon Imtiaz <haroonimtiaz832@gmail.com>
2025-08-25 10:42:53 -0700

    Create if-elif-else-Exam-result-system.py

---
+++ if-elif-else-Exam-result-system.py
@@ -0,0 +1,11 @@
+percentage = int(input("please enter your percentage:"))
+if percentage == 80:
+    print("Distinction")
+elif percentage >= 60:
+    print("First division")
+elif percentage >=  45:
+    print("second division")
+elif percentage >= 33:
+    print("Pass")
+else:
+    print("fail")
