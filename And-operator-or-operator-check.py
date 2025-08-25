d53f04eb737e7c9bc6ff9425baefbfa91575071b
Haroon Imtiaz <haroonimtiaz832@gmail.com>
2025-08-25 11:07:13 -0700

    Create And-operator-or-operator-check.py

---
+++ And-operator-or-operator-check.py
@@ -0,0 +1,5 @@
+number = int(input("Please enter a number:"))
+if number > 10 and number < 50:
+    print("the number is greater than 10 and less than 50")
+else:
+    print("The number is out of range")
