854db441fe3896a1ec63566178d7f091b365639b
Haroon Imtiaz <haroonimtiaz832@gmail.com>
2025-08-26 05:50:33 -0700

    Update For-loop-work-2.py

--- For-loop-work-2.py
+++ For-loop-work-2.py
@@ -1,1 +1,9 @@
-s
+Random = ["0325678807","03407899087","03481233456"]
+for number in Random:
+    if len(number) == 11 and number.startswith("0"):
+        Haroon = number
+        ZeroRemovedVersion = Haroon[1:]
+        CountryCodeAdded = "+92"+ZeroRemovedVersion
+        print(CountryCodeAdded)
+    else:
+        print("Not a valid number")
