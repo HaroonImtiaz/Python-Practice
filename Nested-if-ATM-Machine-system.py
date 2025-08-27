abeb87efa49036920909a605a88223d4d6d3d2d9
Haroon Imtiaz <haroonimtiaz832@gmail.com>
2025-08-26 20:33:22 -0700

    ATM Code Change

--- Nested-if-ATM-Machine-system.py
+++ Nested-if-ATM-Machine-system.py
@@ -1,7 +1,7 @@
 card_valid = input("please enter your valid card no yes/no:")
 if card_valid == "yes":
     pin = int(input("Please enter your four digit pin:"))
-    if pin == 4884:
+    if pin == 4444:
         print("Transaction allowed")
     else:
         print("wrong pin")
