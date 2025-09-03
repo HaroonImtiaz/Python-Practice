3fabc8a08c951d7411c6a269f2a8822fc04e191f
Haroon Imtiaz <haroonimtiaz832@gmail.com>
2025-09-03 08:06:38 -0700

    all

---
+++ practice-mix.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+sum = num1 + num2
+print(sum)
+difference = num1 - num2
+print(difference)
+product = num1 * num2
+print(product)
+division = num1 / num2
+print(division)
---
+++ practice-mix1.py
@@ -0,0 +1,12 @@
+num1 =input("enter your first number:")
+num2 =input("enter your second number:")
+x = int(num1)
+y = int(num2)
+sum = x + y
+print("The sum is :",sum)
+product = x * y
+print("The product is :",product)
+difference = x - y
+print("The difference is :",difference)
+division = x / y
+print("The division is : ",division)
