9ce3c1ec584aa1b13be723cd819f4afef2798ed5
Haroon Imtiaz <haroonimtiaz832@gmail.com>
2025-09-12 08:28:50 -0700

    practice

---
+++ all-math-operation.py
@@ -0,0 +1,16 @@
+num1 = int(input("enter your first number:"))
+num2 = int(input("enter your second number:"))
+sum = num1 + num2
+product = num1 - num2
+multiply = num1 * num2
+division = num1 /num2
+quotient = num1 //num2
+remainder = num1 % num2
+power = num1**num2
+print("The sum is",sum)
+print("The product is",product)
+print("The quotient is",quotient)
+print("The multiplication is",multiply)
+print("The division is",division)
+print("The quotient is",quotient)
+print("The remainder is",remainder)
